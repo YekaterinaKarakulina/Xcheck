ff74ec4ee007008ffd69770ef8741d1290759c5a
Katsiaryna Ramaniuk <Katsiaryna_Ramaniuk@epam.com>
2020-08-29 00:26:36 +0300

    fix: [Tasks list page layout] misspelling in name and route

--- src/sagas/tasksTable.js
+++ src/sagas/tasksTable.js
@@ -7,7 +7,7 @@ import {
 } from '../store/actions/types';
 
 function* workerGetTasksTable() {
-  const uri = 'http://localhost:3000/tasks';
+  const uri = 'http://localhost:3000/tasksTableData';
   try {
     const result = yield call(Axios.get, uri);
     yield put({ type: GET_TASKSTABLE_SESSIONS_SUCCESS, payload: result.data });
