a48e80f7cbd56de9216b2309778542f8e633a8c4
PavelZakharov <pavelzakharov1987@gmail.com>
2020-08-31 00:03:15 +0300

    refactor: [requestFormLayout] change fields and style

--- src/pages/reviewRequest/reviewRequest.jsx
+++ src/pages/reviewRequest/reviewRequest.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Form, /* Input,  Radio, Select, Checkbox, */ Button /* , DatePicker */ } from 'antd';
+import { Form, Input, /* Radio, */ Select, /* Checkbox, */ Button /* , DatePicker */ } from 'antd';
+
+import './reviewRequest.scss';
 
 const FormItem = Form.Item;
+const { Option } = Select;
+const crossCheckSessionData = ['rss2020Q1', 'rss2020Q3react', 'rss2020Q3angular'];
+const tasks = ['simple-task-v1', 'simple-task-v2', 'simple-task-v3'];
+const states = ['draft', 'published', 'completed'];
 
-// const formItemLayout = {
-//   labelCol: {
-//     xs: { span: 24 },
-//     sm: { span: 6 },
-//   },
-//   wrapperCol: {
-//     xs: { span: 24 },
-//     sm: { span: 14 },
-//   },
-// };
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 },
+  },
+};
 
 const tailFormItemLayout = {
   wrapperCol: {
@@ -28,70 +34,108 @@ const tailFormItemLayout = {
   },
 };
 
-// const makeField = (Component) => ({ input, meta, children, hasFeedback, label, ...rest }) => {
-//   console.log(input);
-//   const hasError = meta.touched && meta.invalid;
-//   return (
-//     <FormItem
-//       {...formItemLayout}
-//       label={label}
-//       validateStatus={hasError ? 'error' : 'success'}
-//       hasFeedback={hasFeedback && hasError}
-//       help={hasError && meta.error}
-//     >
-//       <Component {...input} {...rest} children={children} />
-//     </FormItem>
-//   );
-// };
+const makeField = (Component) => ({ /* input, meta, children, hasFeedback, label, */ ...rest }) => {
+  // console.log(rest);
+  // const hasError = meta.touched && meta.invalid;
+  return (
+    <FormItem
+      {...formItemLayout}
+      label={rest.label}
+      // validateStatus={hasError ? 'error' : 'success'}
+      // hasFeedback={hasFeedback && hasError}
+      // help={hasError && meta.error}
+    >
+      <Component /* {...input} */ {...rest} /* children={children} */ />
+    </FormItem>
+  );
+};
 
-// const AInput = makeField(Input);
+const AInput = makeField(Input);
+const ASelect = makeField(Select);
 
 const ReviewRequest = (props) => {
   //   const { handleSubmit, pristine, reset, submitting } = props;
   console.log(props);
   return (
-    <Form /* onSubmit={handleSubmit} */>
-      <Field
-        label="First Name"
-        name="firstName"
-        component="input" /* {AInput} */
-        placeholder="First Name"
-        hasFeedback
-      />
-
-      <Field
-        label="Last Name"
-        name="lastName"
-        component="input" /* {AInput} */
-        placeholder="Last Name"
-      />
-
-      <Field
-        label="Email"
-        name="email"
-        component="input" /* {AInput} */
-        type="email"
-        placeholder="Email"
-      />
-
-      <FormItem {...tailFormItemLayout}>
-        <Button
-          type="primary"
-          //   disabled={pristine || submitting}
-          htmlType="submit"
-          style={{ marginRight: '10px' }}
+    <div className="review__request">
+      <Form /* onSubmit={handleSubmit} */>
+        <Field
+          defaultValue={crossCheckSessionData[0]}
+          label="crossCheckSession"
+          name="crossCheckSession"
+          component={ASelect}
         >
-          Submit
-        </Button>
-      </FormItem>
-    </Form>
+          {crossCheckSessionData.map((crossCheckSession) => (
+            <Option key={crossCheckSession} value={crossCheckSession}>
+              {crossCheckSession}
+            </Option>
+          ))}
+        </Field>
+
+        <Field
+          label="Author"
+          name="Author"
+          component={AInput}
+          placeholder="Add your name"
+          // hasFeedback
+        />
+
+        <Field defaultValue={tasks[0]} label="task" name="task" component={ASelect}>
+          {tasks.map((task) => (
+            <Option key={task} value={task}>
+              {task}
+            </Option>
+          ))}
+        </Field>
+
+        <Field defaultValue={states[0]} label="state" name="state" component={ASelect}>
+          {states.map((state) => (
+            <Option key={state} value={state}>
+              {state}
+            </Option>
+          ))}
+        </Field>
+
+        <Field
+          label="linkToDemo"
+          name="linkToDemo"
+          component={AInput}
+          placeholder="Add link for demo"
+        />
+
+        <Field
+          label="linkToPR"
+          name="linkToPR"
+          component={AInput}
+          placeholder="Add link for Rull Request"
+        />
+
+        <Field
+          label="selfGrade"
+          name="selfGrade"
+          component={AInput}
+          placeholder="Add number of score"
+        />
+
+        <FormItem {...tailFormItemLayout}>
+          <Button
+            type="primary"
+            //   disabled={pristine || submitting}
+            htmlType="submit"
+            style={{ marginRight: '10px' }}
+          >
+            Submit
+          </Button>
+        </FormItem>
+      </Form>
+    </div>
   );
 };
 
 const validate = (values) => {
   const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
+  if (!values.Author) {
+    errors.Author = 'Required';
   }
 
   return errors;
