75c28e0e8a2d6e6ccad6f40e0c9808618b35733f
Gunel Huseynova <gorellik246@gmail.com>
2020-09-08 22:47:15 +0300

    fix:[checkPageLayout] git merge conflicts

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ReviewRequest from '../pages/reviewRequest';
 import {
   CrossCheckSessionsTable,
@@ -14,33 +14,37 @@ import {
   TaskFormEdit,
 } from '../pages';
 
-const Routes = () => {
+const Routes = ({ isLoggedIn }) => {
   return (
     <Switch>
-      <>
-        <Route path="/login" component={GithubLogin} />
-        {/* <Redirect to="/login" /> */}
-
-        <Route path="/tasks" component={TasksTable} />
-        <Route path="/check" component={Check} />
-        <Route path="/requests" component={Requests} />
-        <Route path="/review-request" component={ReviewRequest} />
-        <Route path="/reviews" component={ReviewsListPage} />
-        <Route path="/task-form" component={TaskForm} />
-        <Route path="/task-edit-form" component={TaskFormEdit} />
-        {/* <Redirect to="/reviews" /> */}
-        <Route exact path="/cross-check-sessions" component={CrossCheckSessionsTable} />
-        <Route
-          path="/cross-check-sessions/cross-check-session-form"
-          component={CrossCheckSessionForm}
-        />
-      </>
+      {!isLoggedIn ? (
+        <>
+          <Route path="/login" component={GithubLogin} />
+          <Redirect to="/login" />
+        </>
+      ) : (
+        <>
+          <Route path="/tasks" component={TasksTable} />
+          <Route path="/check" component={Check} />
+          <Route path="/requests" component={Requests} />
+          <Route path="/review-request" component={ReviewRequest} />
+          <Route path="/reviews" component={ReviewsListPage} />
+          <Route path="/task-form" component={TaskForm} />
+          <Route path="/task-edit-form" component={TaskFormEdit} />
+          <Redirect to="/reviews" />
+          <Route exact path="/cross-check-sessions" component={CrossCheckSessionsTable} />
+          <Route
+            path="/cross-check-sessions/cross-check-session-form"
+            component={CrossCheckSessionForm}
+          />
+        </>
+      )}
     </Switch>
   );
 };
 
-// Routes.propTypes = {
-//   isLoggedIn: PropTypes.bool.isRequired,
-// };
+Routes.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
 
 export default React.memo(Routes);
