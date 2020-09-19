bc9ca9b0fbcc5dcc48567d35700df4a35b692889
Kipris <tnzaria@gmail.com>
2020-09-19 13:40:57 +0300

    fix: move server file

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.set("port", (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 3001));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
-  return res.send("pong");
-});
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
