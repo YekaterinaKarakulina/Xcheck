72478c57a82a9b23e433dd7ddd469da09f456f1d
Kipris <tnzaria@gmail.com>
2020-09-19 13:09:15 +0300

    fix: improve server for deploying

--- server/index.js
+++ server/index.js
@@ -4,5 +4,12 @@ const path = require("path");
 const app = express();
 
 app.set("port", (process.env.PORT || 5000));
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(app.get("port"), () => console.log("Listening on Port", app.get("port")));
