fb9add4c9cd1d6235251be7002fe5ac8eedba3d5
YEKATERINA KARAKULINA <ekaterina.jj@gmail.com>
2020-09-21 15:05:14 +0400

    refactor: [CrossCheck Sessions] ttable compinents refactoring

--- src/components/cross-check-sessions-table/cross-check-sessions-table-creation.jsx
+++ src/components/cross-check-sessions-table/cross-check-sessions-table-creation.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Table, Tag, Space } from 'antd';
-import { EyeTwoTone, EditTwoTone, CloseCircleTwoTone, InfoCircleTwoTone } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import {
   getCrossCheckSession,
@@ -11,12 +9,11 @@ import {
   closeModal,
 } from '../../store/actions/cross-check-session';
 import { getTask, getTaskByTitle } from '../../store/actions/task';
-import checkStatus from '../../utils/status';
-import { CrossCheckSessionModal } from '../modals';
+import CrossCheckSessionsTable from './cross-check-sessions-table';
 
 import './cross-check-sessions-table-creation.scss';
 
-class CrossCheckSessionsTableCreation extends React.Component {
+class CrossCheckSessionsTableContainer extends React.Component {
   componentDidUpdate(prev) {
     const { currentTask, history, getTask } = this.props;
     const currentTaskId = currentTask.taskId;
@@ -29,6 +26,7 @@ class CrossCheckSessionsTableCreation extends React.Component {
 
   render() {
     const {
+      getTaskByTitle,
       getCrossCheckSession,
       deleteCrossCheckSession,
       history,
@@ -39,117 +37,23 @@ class CrossCheckSessionsTableCreation extends React.Component {
       tableData,
     } = this.props;
 
-    const redirectToTask = async (taskTitle) => {
-      const { getTaskByTitle } = this.props;
-      await getTaskByTitle(taskTitle);
-    };
-
-    const columns = [
-      {
-        title: 'Title',
-        dataIndex: 'title',
-        key: 'title',
-      },
-      {
-        title: 'Author',
-        dataIndex: 'author',
-        key: 'author',
-      },
-      {
-        title: 'Status',
-        key: 'state',
-        dataIndex: 'state',
-        render: (state) => {
-          const color = checkStatus(state);
-          return <Tag color={color}>{state.toUpperCase()}</Tag>;
-        },
-      },
-      {
-        title: 'Task',
-        dataIndex: 'taskTitle',
-        key: 'taskTitle',
-        render: (action, row) => {
-          return (
-            <span>
-              {row.taskTitle}
-              <InfoCircleTwoTone
-                onClick={() => redirectToTask(row.taskTitle)}
-                style={{ marginLeft: '0.5rem' }}
-              />
-            </span>
-          );
-        },
-      },
-      {
-        title: 'Coefficient',
-        dataIndex: 'coefficient',
-        key: 'coefficient',
-      },
-      {
-        title: 'Start date',
-        dataIndex: 'startDate',
-        key: 'startDate',
-      },
-      {
-        title: 'End date',
-        dataIndex: 'endDate',
-        key: 'endDate',
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        render: (action, row) => {
-          return (
-            <>
-              <Space size="middle" data-id={row.id}>
-                <EyeTwoTone
-                  twoToneColor="#9254de"
-                  onClick={() => {
-                    history.push(row.id);
-                  }}
-                />
-
-                <EditTwoTone
-                  twoToneColor="#ffa940"
-                  className={row.state === 'closed' ? 'disabled' : ''}
-                  onClick={
-                    row.state === 'closed'
-                      ? () => {}
-                      : async () => {
-                          await getCrossCheckSession(row.key);
-                          history.push('cross-check-session-edit-form');
-                        }
-                  }
-                />
-                <CloseCircleTwoTone
-                  twoToneColor="#ff4d4f"
-                  onClick={() => {
-                    let isAbleToDelete = true;
-                    reviewRequestsData.forEach((reviewRequest) => {
-                      if (reviewRequest.crossCheckSessionId === row.key) {
-                        isAbleToDelete = false;
-                      }
-                    });
-                    if (isAbleToDelete) {
-                      deleteCrossCheckSession(row.key);
-                    } else {
-                      openModal();
-                    }
-                  }}
-                />
-              </Space>
-              <CrossCheckSessionModal isModalVisible={isModalVisible} closeModal={closeModal} />
-            </>
-          );
-        },
-      },
-    ];
-
-    return <Table columns={columns} dataSource={tableData} />;
+    return (
+      <CrossCheckSessionsTable
+        getTaskByTitle={getTaskByTitle}
+        getCrossCheckSession={getCrossCheckSession}
+        deleteCrossCheckSession={deleteCrossCheckSession}
+        history={history}
+        reviewRequestsData={reviewRequestsData}
+        isModalVisible={isModalVisible}
+        openModal={openModal}
+        closeModal={closeModal}
+        tableData={tableData}
+      />
+    );
   }
 }
 
-CrossCheckSessionsTableCreation.propTypes = {
+CrossCheckSessionsTableContainer.propTypes = {
   tableData: PropTypes.instanceOf(Array).isRequired,
   getCrossCheckSession: PropTypes.func.isRequired,
   deleteCrossCheckSession: PropTypes.func.isRequired,
@@ -163,7 +67,7 @@ CrossCheckSessionsTableCreation.propTypes = {
   currentTask: PropTypes.instanceOf(Object),
 };
 
-CrossCheckSessionsTableCreation.defaultProps = {
+CrossCheckSessionsTableContainer.defaultProps = {
   currentTask: {},
 };
 
@@ -185,5 +89,5 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(CrossCheckSessionsTableCreation)
+  connect(mapStateToProps, mapDispatchToProps)(CrossCheckSessionsTableContainer)
 );
