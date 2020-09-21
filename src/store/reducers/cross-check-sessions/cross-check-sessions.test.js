44d3a4c3e943180cfc35b6bb435f9db5db135776
YEKATERINA KARAKULINA <ekaterina.jj@gmail.com>
2020-09-21 09:32:22 +0400

    feat: [unit tests] add cross-check-sessions reducers tests

--- src/store/reducers/cross-check-sessions/cross-check-sessions.test.js
+++ src/store/reducers/cross-check-sessions/cross-check-sessions.test.js
@@ -5,13 +5,77 @@ const initialState = {
   currentSessionInfo: {},
 };
 
-describe('crossCheckSessions', () => {
-    test('modal open', () => {
-      const result = crossCheckSessions(initialState, {type: 'OPEN_MODAL'});
-      expect(result.isModalVisible).toEqual(true);
+describe('Modal tests', () => {
+test('modal open', () => {
+    const result = crossCheckSessions(initialState, {type: 'OPEN_MODAL'});
+    expect(result.isModalVisible).toEqual(true);
+  })
+test('modal close', () => {
+    const result = crossCheckSessions(initialState, {type: 'CLOSE_MODAL'});
+    expect(result.isModalVisible).toEqual(false);
+  })
+});
+
+describe('Get cross check session', () => {
+  const crossCheckSession = {
+    "id": "xcheck-1",
+    "title": "rss2020Q3react-songbird",
+    "author": "viktorsipach",
+    "state": "draft",
+    "taskTitle": "Task 1",
+    "crossCheckSessionPeriod": [
+      "2020-07-08",
+      "2020-07-14"
+    ],
+    "coefficient": 0.2,
+    "discardMinScore": true,
+    "discardMaxScore": true,
+    "minReviewsAmount": 1,
+    "desiredReviewsAmount": 2,
+    "attendees": []
+  }
+test('success', () => {
+   const result = crossCheckSessions(initialState, {type: 'GET_CROSSCHECK_SESSION_SUCCESS', payload: crossCheckSession});
+      expect(result.currentSessionInfo).toEqual(crossCheckSession);
     })
-    test('modal close', () => {
-      const result = crossCheckSessions(initialState, {type: 'CLOSE_MODAL'});
-      expect(result.isModalVisible).toEqual(false);
-    })
-  });
+test('failure', () => {
+    const result = crossCheckSessions(initialState, {type: 'GET_CROSSCHECK_SESSION_FAILURE'});
+    expect(result.currentSessionInfo).toEqual({});
+  })
+});
+
+describe('Post cross check session', () => {
+test('success', () => {
+    const result = crossCheckSessions(initialState, {type: 'POST_CROSSCHECK_SESSION_SUCCESS'});
+    expect(result).toBeInstanceOf(Object);
+    expect(result.isModalVisible).toBeFalsy();
+  })
+test('failure', () => {
+    const result = crossCheckSessions(initialState, {type: 'POST_CROSSCHECK_SESSION_FAILURE'});
+    expect(result.currentSessionInfo).toBeTruthy();
+  })
+});
+
+describe('Update cross check session', () => {
+test('success', () => {
+    const result = crossCheckSessions(initialState, {type: 'UPDATE_CROSSCHECK_SESSION_SUCCESS'});
+    expect(result).toBeInstanceOf(Object);
+    expect(result.isModalVisible).toBeFalsy();
+  })
+test('failure', () => {
+    const result = crossCheckSessions(initialState, {type: 'UPDATE_CROSSCHECK_SESSION_FAILURE'});
+    expect(result.currentSessionInfo).toBeTruthy();
+  })
+});
+
+describe('Delete cross check session', () => {
+test('success', () => {
+    const result = crossCheckSessions(initialState, {type: 'DELETE_CROSSCHECK_SESSION_SUCCESS'});
+    expect(result).toBeInstanceOf(Object);
+    expect(result.isModalVisible).toBeFalsy();
+  })
+test('failure', () => {
+    const result = crossCheckSessions(initialState, {type: 'DELETE_CROSSCHECK_SESSION_FAILURE'});
+    expect(result.currentSessionInfo).toBeTruthy();
+  })
+});
