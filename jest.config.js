8666433131e03e2e065ddadf23ab36763e0b4caf
YEKATERINA KARAKULINA <ekaterina.jj@gmail.com>
2020-09-23 09:26:14 +0400

    feat: [unit tests] add babel-jest, update jest config

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,9 @@ module.exports = {
       'js',
       'jsx',
     ],
-    setupFilesAfterEnv: ['./jest.setup.js']
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    moduleNameMapper: {
+      "^.+\\.(css|less|scss)$": "babel-jest"
+    }
   };
   
