fee28d287170b241961c20224a1ed3875437d5b3
PavelZakharov <pavelzakharov1987@gmail.com>
2020-09-23 20:54:54 +0300

    fix: [ReviewRequestsTests] add test-data file

--- src/store/reducers/review-requests/review-requests.test.js
+++ src/store/reducers/review-requests/review-requests.test.js
@@ -1,76 +1,48 @@
 import reviewRequests from './review-requests';
-
-const initialState = {
-  isModalVisible: false,
-  currentReviewRequest: {},
-};
-
-const reviewRequest = {
-  "id": "rev-req-4",
-  "author": "viktorsipach",
-  "taskTitle": "Task 3",
-  "state": "readyToXCheck",
-  "selfGrade": {
-    "task-3-bp1": {
-      "score": 20,
-      "comment": "Well done!"
-    },
-    "task-3-ep1": {
-      "score": 15,
-      "comment": "Some things are done, some are not"
-    },
-    "task-3-fp1": {
-      "score": 0,
-      "comment": "No ticket today"
-    }
-  },
-  "linkToDemo": "#",
-  "linkToPR": "#",
-  "crossCheckSessionId": "xcheck-3"
-};
+import {initialStateFull, reviewRequest } from './test-data';
 
 describe('reviewRequests', () => {
     test('modal open', () => {
-      const result = reviewRequests(initialState, {type: 'OPEN_MODAL'});
+      const result = reviewRequests(initialStateFull, {type: 'OPEN_MODAL'});
       expect(result.isModalVisible).toEqual(true);
     })
     test('modal close', () => {
-      const result = reviewRequests(initialState, {type: 'CLOSE_MODAL'});
+      const result = reviewRequests(initialStateFull, {type: 'CLOSE_MODAL'});
       expect(result.isModalVisible).toEqual(false);
     })
   });
 
 describe('Get review request', () => {
   test('success', () => {
-    const result = reviewRequests(initialState, {type: 'GET_REVIEW_REQUESTS_SUCCESS', payload: reviewRequest});
+    const result = reviewRequests(initialStateFull, {type: 'GET_REVIEW_REQUESTS_SUCCESS', payload: reviewRequest});
         expect(result.currentReviewRequest).toEqual(reviewRequest);
     })
   test('failure', () => {
-    const result = reviewRequests(initialState, {type: 'GET_REVIEW_REQUESTS_FAILURE'});
+    const result = reviewRequests(initialStateFull, {type: 'GET_REVIEW_REQUESTS_FAILURE'});
     expect(result.currentReviewRequest).toEqual({});
   })
 });
 
 describe('Post review request', () => {
   test('success', () => {
-    const result = reviewRequests(initialState, {type: 'POST_REVIEW_REQUEST_SUCCESS'});
+    const result = reviewRequests(initialStateFull, {type: 'POST_REVIEW_REQUEST_SUCCESS'});
     expect(result).toBeInstanceOf(Object);
     expect(result.isModalVisible).toBeFalsy();
   })
   test('failure', () => {
-    const result = reviewRequests(initialState, {type: 'POST_REVIEW_REQUEST_FAILURE'});
+    const result = reviewRequests(initialStateFull, {type: 'POST_REVIEW_REQUEST_FAILURE'});
     expect(result.currentReviewRequest).toBeTruthy();
   })
 });
 
 describe('Update review request', () => {
   test('success', () => {
-    const result = reviewRequests(initialState, {type: 'UPDATE_REVIEW_REQUEST_SUCCESS'});
+    const result = reviewRequests(initialStateFull, {type: 'UPDATE_REVIEW_REQUEST_SUCCESS'});
     expect(result).toBeInstanceOf(Object);
     expect(result.isModalVisible).toBeFalsy();
   })
   test('failure', () => {
-    const result = reviewRequests(initialState, {type: 'UPDATE_REVIEW_REQUEST_FAILURE'});
+    const result = reviewRequests(initialStateFull, {type: 'UPDATE_REVIEW_REQUEST_FAILURE'});
     expect(result.currentReviewRequest).toBeTruthy();
   })
 });
