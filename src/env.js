a4e3b4ac04a65ab73ac218f34958692ac078c1f0
Kipris <tnzaria@gmail.com>
2020-09-27 19:12:14 +0300

    merge: develop -> deploy

--- src/env.js
+++ src/env.js
@@ -1,17 +1,17 @@
 // dev
-const env = {
-  clientId: '3cdd93c64851d7e52a5d',
-  dbBaseURL: 'http://localhost:3000/',
-  authBaseURL: 'http://localhost:5000/',
-  appBaseURL: 'http://localhost:8080/',
-};
+// const env = {
+//   clientId: '3cdd93c64851d7e52a5d',
+//   dbBaseURL: 'http://localhost:3000/',
+//   authBaseURL: 'http://localhost:5000/',
+//   appBaseURL: 'http://localhost:8080/',
+// };
 
 // prod
-// const env = {
-//   clientId: 'ee289f1f42213c220a96',
-//   dbBaseURL: 'https://xcheck-team11-db.herokuapp.com/',
-//   authBaseURL: 'https://xcheck-team11-auth.herokuapp.com/',
-//   appBaseURL: 'https://xcheck-team11.herokuapp.com/',
-// };
+const env = {
+  clientId: 'ee289f1f42213c220a96',
+  dbBaseURL: 'https://xcheck-team11-db.herokuapp.com/',
+  authBaseURL: 'https://xcheck-team11-auth.herokuapp.com/',
+  appBaseURL: 'https://xcheck-team11.herokuapp.com/',
+};
 
 export default env;
